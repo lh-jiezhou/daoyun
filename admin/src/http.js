4b69e61daf7919f230a3a20c83a63086fc22dae1
lh-jiezhou <897587636@qq.com>
2020-04-13 12:34:15 +0800

    添加egg服务端http

--- admin/src/http.js
+++ admin/src/http.js
@@ -4,5 +4,12 @@ const http = axios.create({
     baseURL: 'http://localhost:3000/admin/api'
 })
 
+const http_egg = axios.create({
+    baseURL: 'http://127.0.0.1:7001/admin/api'
+})
+
 //导出http变量
-export default http
+export default {
+    http,
+    http_egg
+}
